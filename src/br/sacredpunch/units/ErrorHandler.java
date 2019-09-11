bc5713eb98547ddc01a0c99c01c55c8c604796e7
lucas <lucas@DESKTOP-MGDLCF1>
2019-09-11 10:36:19 -0300

    Atualizei os códigos nas Classes ErrorHandler e Token.

--- src/br/sacredpunch/units/ErrorHandler.java
+++ src/br/sacredpunch/units/ErrorHandler.java
@@ -1,5 +1,12 @@
 package br.sacredpunch.units;
 
 public class ErrorHandler {
-
+	private  static ErrorHandler instance = new ErrorHandler();	
+	
+	public static ErrorHandler getInstance() {
+		return instance;
+	}
+	public void printErrorReport() {
+		
+	}
 }
--- src/br/sacredpunch/units/Token.java
+++ src/br/sacredpunch/units/Token.java
@@ -36,4 +36,8 @@ public class Token {
 		//Configurar os atributos
 	}
 	
+	public void printToken() {
+		
+	}
+	
 }
